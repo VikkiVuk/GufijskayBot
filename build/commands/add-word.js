1f07a0a713d3510582ffe7daa7c26231b862dc8b
NisamVikki <viktorblazaric2@gmail.com>
2022-11-28 19:24:22 +0100

    Improved add-word.js
    Improved the command /add-word so that it shows a modal instead of a normal command.

--- build/commands/add-word.js
+++ build/commands/add-word.js
@@ -15,91 +15,126 @@ const google_spreadsheet_1 = require("google-spreadsheet");
 module.exports = {
     data: new builders_1.SlashCommandBuilder()
         .setName('add-word')
-        .setDescription('Add a word to gufijskay jøzik')
-        .addStringOption(option => option.setName('gufijskay').setDescription('The word to add in gufijskay').setRequired(true))
-        .addStringOption(option => option.setName('english').setDescription('The word to add in english').setRequired(true))
-        .addStringOption(option => option.setName('description').setDescription('The description of the word').setRequired(false)),
+        .setDescription('Add a word to gufijskay jøzik'),
+    // .addStringOption(option => option.setName('gufijskay').setDescription('The word to add in gufijskay').setRequired(true))
+    // .addStringOption(option => option.setName('english').setDescription('The word to add in english').setRequired(true))
+    // .addStringOption(option => option.setName('description').setDescription('The description of the word').setRequired(false)),
     execute(interaction) {
         return __awaiter(this, void 0, void 0, function* () {
-            yield interaction.deferReply();
-            // check if the user is VikkiVuk#0001 or X Master Woo#5269
-            if (interaction.user.id !== "750725036096094208" && interaction.user.id !== "429331889619337218") {
-                yield interaction.editReply({ content: "You do not have permission to use this command." });
-                return;
-            }
-            const doc = new google_spreadsheet_1.GoogleSpreadsheet(process.env.CAVEMEN_WORDS_SID);
-            yield doc.useServiceAccountAuth({
-                client_email: process.env.CLIENT_EMAIL,
-                private_key: process.env.PRIVATE_KEY,
-            });
-            yield doc.loadInfo();
-            const sheet = doc.sheetsByTitle["Cave man Gufijskay"];
-            const rows = yield sheet.getRows();
-            let found = false;
-            yield rows.forEach((row) => {
-                // @ts-ignore
-                let data = row["_rawData"];
-                let searchableData = [data[0], data[1]];
-                // @ts-ignore
-                if (searchableData[0].toString().toLowerCase() === interaction.options.getString('gufijskay').toLowerCase() || searchableData[1].toString().toLowerCase() === interaction.options.getString('english').toLowerCase()) {
-                    found = true;
+            // reply with a modal
+            const modal = new discord_js_1.ModalBuilder()
+                .setTitle('Add a word to gufijskay jøzik')
+                .setCustomId('word-add');
+            const input1 = new discord_js_1.TextInputBuilder()
+                .setCustomId('gufijskay')
+                .setPlaceholder('Gufijskay')
+                .setStyle(discord_js_1.TextInputStyle.Short)
+                .setLabel('Gufijskay');
+            const input2 = new discord_js_1.TextInputBuilder()
+                .setCustomId('english')
+                .setPlaceholder('English')
+                .setStyle(discord_js_1.TextInputStyle.Short)
+                .setLabel('English');
+            const input3 = new discord_js_1.TextInputBuilder()
+                .setCustomId('description')
+                .setPlaceholder('Description')
+                .setStyle(discord_js_1.TextInputStyle.Paragraph)
+                .setLabel('Description')
+                .setRequired(false);
+            const inputRow = new discord_js_1.ActionRowBuilder().addComponents(input1);
+            const inputRow2 = new discord_js_1.ActionRowBuilder().addComponents(input2);
+            const inputRow3 = new discord_js_1.ActionRowBuilder().addComponents(input3);
+            // @ts-ignore
+            modal.addComponents(inputRow, inputRow2, inputRow3);
+            yield interaction.showModal(modal);
+            // filter for the modal interaction
+            const filter = (i) => i.customId === 'word-add' && i.user.id === interaction.user.id;
+            yield interaction.awaitModalSubmit({ filter: filter, time: 30000 }).then((modalInteraction) => __awaiter(this, void 0, void 0, function* () {
+                yield modalInteraction.deferReply();
+                // check if the user is VikkiVuk#0001 or X Master Woo#5269
+                if (modalInteraction.user.id !== "750725036096094208" && modalInteraction.user.id !== "429331889619337218") {
+                    yield modalInteraction.editReply({ content: "You do not have permission to use this command." });
+                    return;
                 }
-            });
-            if (found) {
-                yield interaction.editReply({ content: `Word is already in the language` });
-            }
-            else {
-                // send an embed with a button to confirm the word
-                const embed = new discord_js_1.EmbedBuilder()
-                    .setTitle('Confirm Word') // @ts-ignore
-                    .setDescription(`Are you sure you want to add ${interaction.options.getString('gufijskay')}(${interaction.options.getString('english')}) to the language?`)
-                    .setColor(0x00ff00);
-                // make a button to confirm the word
-                const button = new discord_js_1.ButtonBuilder()
-                    .setCustomId('confirm')
-                    .setLabel('Confirm')
-                    .setStyle(discord_js_1.ButtonStyle.Success);
-                // make a button to deny the word
-                const button2 = new discord_js_1.ButtonBuilder()
-                    .setCustomId('cancel')
-                    .setLabel('Cancel')
-                    .setStyle(discord_js_1.ButtonStyle.Secondary);
-                // make a row for the buttons
-                const buttonRow = new discord_js_1.ActionRowBuilder().addComponents(button, button2);
-                // @ts-ignore
-                yield interaction.editReply({ embeds: [embed], components: [buttonRow], fetchReply: true }).then((msg) => __awaiter(this, void 0, void 0, function* () {
+                const doc = new google_spreadsheet_1.GoogleSpreadsheet(process.env.CAVEMEN_WORDS_SID);
+                yield doc.useServiceAccountAuth({
+                    client_email: process.env.CLIENT_EMAIL,
+                    private_key: process.env.PRIVATE_KEY,
+                });
+                yield doc.loadInfo();
+                const sheet = doc.sheetsByTitle["Cave man Gufijskay"];
+                const rows = yield sheet.getRows();
+                // get the values from the modal
+                const gufijskay = modalInteraction.fields.getTextInputValue('gufijskay');
+                const english = modalInteraction.fields.getTextInputValue('english');
+                const description = modalInteraction.fields.getTextInputValue('description');
+                let found = false;
+                yield rows.forEach((row) => {
                     // @ts-ignore
-                    const filter = (interaction) => interaction.user.id === interaction.user.id;
+                    let data = row["_rawData"];
+                    let searchableData = [data[0], data[1]];
                     // @ts-ignore
-                    const collector = msg.createMessageComponentCollector({ filter, time: 30000 });
-                    // @ts-ignore
-                    let toAdd = [`${interaction.options.getString('gufijskay')}`, `${interaction.options.getString('english')}`];
-                    // @ts-ignore
-                    if (interaction.options.getString('description')) {
-                        // @ts-ignore
-                        toAdd = [...toAdd, `${interaction.options.getString('description')}`];
+                    if (searchableData[0].toString().toLowerCase() === gufijskay.toLowerCase() || searchableData[1].toString().toLowerCase() === english.toLowerCase()) {
+                        found = true;
                     }
-                    let row = yield sheet.addRow(toAdd);
-                    collector.on('collect', (i) => __awaiter(this, void 0, void 0, function* () {
-                        if (i.customId === 'confirm') {
-                            yield row.save();
-                            yield interaction.editReply({ content: `Added ${row._rawData[0]} to the language`, components: [], embeds: [] });
-                            collector.stop("confirmed");
+                });
+                if (found) {
+                    yield modalInteraction.editReply({ content: "The word already exists in the dictionary." });
+                    return;
+                }
+                else {
+                    // send an embed with a button to confirm the word
+                    const embed = new discord_js_1.EmbedBuilder()
+                        .setTitle('Confirm Word') // @ts-ignore
+                        .setDescription(`Are you sure you want to add ${gufijskay}(${english}) to the language?`)
+                        .setColor(0x00ff00);
+                    // make a button to confirm the word
+                    const button = new discord_js_1.ButtonBuilder()
+                        .setCustomId('confirm')
+                        .setLabel('Confirm')
+                        .setStyle(discord_js_1.ButtonStyle.Success);
+                    // make a button to deny the word
+                    const button2 = new discord_js_1.ButtonBuilder()
+                        .setCustomId('cancel')
+                        .setLabel('Cancel')
+                        .setStyle(discord_js_1.ButtonStyle.Secondary);
+                    // make a row for the buttons
+                    const buttonRow = new discord_js_1.ActionRowBuilder().addComponents(button, button2);
+                    // @ts-ignore
+                    yield modalInteraction.editReply({ embeds: [embed], components: [buttonRow], fetchReply: true }).then((msg) => __awaiter(this, void 0, void 0, function* () {
+                        // @ts-ignore
+                        const filter = (modalInteraction) => modalInteraction.user.id === modalInteraction.user.id;
+                        // @ts-ignore
+                        const collector = msg.createMessageComponentCollector({ filter, time: 30000 });
+                        // @ts-ignore
+                        let toAdd = [`${gufijskay}`, `${english}`];
+                        // @ts-ignore
+                        if (description) {
+                            // @ts-ignore
+                            toAdd = [...toAdd, `${description}`];
                         }
-                        else if (i.customId === 'cancel') {
+                        let row = yield sheet.addRow(toAdd);
+                        collector.on('collect', (i) => __awaiter(this, void 0, void 0, function* () {
+                            if (i.customId === 'confirm') {
+                                yield row.save();
+                                yield modalInteraction.editReply({ content: `Added ${row._rawData[0]} to the language`, components: [], embeds: [] });
+                                collector.stop("confirmed");
+                            }
+                            else if (i.customId === 'cancel') {
+                                yield row.delete();
+                                yield modalInteraction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
+                                collector.stop("cancelled");
+                            }
+                        }));
+                        collector.on('end', (collected, reason) => __awaiter(this, void 0, void 0, function* () {
+                            if (reason === "confirmed" || reason === "cancelled")
+                                return;
                             yield row.delete();
-                            yield interaction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
-                            collector.stop("cancelled");
-                        }
+                            yield modalInteraction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
+                        }));
                     }));
-                    collector.on('end', (collected, reason) => __awaiter(this, void 0, void 0, function* () {
-                        if (reason === "confirmed" || reason === "cancelled")
-                            return;
-                        yield row.delete();
-                        yield interaction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
-                    }));
-                }));
-            }
+                }
+            }));
         });
     },
 };
--- src/commands/add-word.ts
+++ src/commands/add-word.ts
@@ -5,7 +5,11 @@ import {
     ButtonBuilder,
     ActionRowBuilder,
     ButtonStyle,
-    MessageActionRowComponent, ActionRowComponent
+    MessageActionRowComponent,
+    ActionRowComponent,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle, ModalSubmitInteraction
 } from "discord.js";
 // @ts-ignore
 import { GoogleSpreadsheet, GoogleSpreadsheetRow } from "google-spreadsheet";
@@ -13,101 +17,147 @@ import { GoogleSpreadsheet, GoogleSpreadsheetRow } from "google-spreadsheet";
 export = {
     data: new SlashCommandBuilder()
         .setName('add-word')
-        .setDescription('Add a word to gufijskay jøzik')
-        .addStringOption(option => option.setName('gufijskay').setDescription('The word to add in gufijskay').setRequired(true))
-        .addStringOption(option => option.setName('english').setDescription('The word to add in english').setRequired(true))
-        .addStringOption(option => option.setName('description').setDescription('The description of the word').setRequired(false)),
+        .setDescription('Add a word to gufijskay jøzik'),
+        // .addStringOption(option => option.setName('gufijskay').setDescription('The word to add in gufijskay').setRequired(true))
+        // .addStringOption(option => option.setName('english').setDescription('The word to add in english').setRequired(true))
+        // .addStringOption(option => option.setName('description').setDescription('The description of the word').setRequired(false)),
 
     async execute(interaction: CommandInteraction) {
-        await interaction.deferReply();
+        // reply with a modal
+        const modal = new ModalBuilder()
+            .setTitle('Add a word to gufijskay jøzik')
+            .setCustomId('word-add')
 
-        if (interaction.user.id !== "750725036096094208" && interaction.user.id !== "429331889619337218") {
-            await interaction.editReply({ content: "You do not have permission to use this command." });
-            return;
-        }
+        const input1 = new TextInputBuilder()
+            .setCustomId('gufijskay')
+            .setPlaceholder('Gufijskay')
+            .setStyle(TextInputStyle.Short)
+            .setLabel('Gufijskay')
 
-        const doc = new GoogleSpreadsheet(process.env.CAVEMEN_WORDS_SID);
-        await doc.useServiceAccountAuth({
-            client_email: process.env.CLIENT_EMAIL,
-            private_key: process.env.PRIVATE_KEY,
-        });
+        const input2 = new TextInputBuilder()
+            .setCustomId('english')
+            .setPlaceholder('English')
+            .setStyle(TextInputStyle.Short)
+            .setLabel('English')
 
-        await doc.loadInfo();
-        const sheet = doc.sheetsByTitle["Cave man Gufijskay"];
-        const rows = await sheet.getRows();
+        const input3 = new TextInputBuilder()
+            .setCustomId('description')
+            .setPlaceholder('Description')
+            .setStyle(TextInputStyle.Paragraph)
+            .setLabel('Description')
+            .setRequired(false)
 
-        let found = false;
-        await rows.forEach((row: string) => {
-            // @ts-ignore
-            let data = row["_rawData"]
-            let searchableData = [ data[0], data[1] ]
+        const inputRow = new ActionRowBuilder().addComponents(input1)
+        const inputRow2 = new ActionRowBuilder().addComponents(input2)
+        const inputRow3 = new ActionRowBuilder().addComponents(input3)
 
-            // @ts-ignore
-            if (searchableData[0].toString().toLowerCase() === interaction.options.getString('gufijskay').toLowerCase() || searchableData[1].toString().toLowerCase() === interaction.options.getString('english').toLowerCase()) {
-                found = true
+        // @ts-ignore
+        modal.addComponents(inputRow, inputRow2, inputRow3)
+
+        await interaction.showModal(modal)
+
+        // filter for the modal interaction
+        const filter = (i: ModalSubmitInteraction) => i.customId === 'word-add' && i.user.id === interaction.user.id
+
+        await interaction.awaitModalSubmit({filter: filter, time: 30000}).then(async (modalInteraction: ModalSubmitInteraction) => {
+            await modalInteraction.deferReply();
+
+            // check if the user is VikkiVuk#0001 or X Master Woo#5269
+            if (modalInteraction.user.id !== "750725036096094208" && modalInteraction.user.id !== "429331889619337218") {
+                await modalInteraction.editReply({ content: "You do not have permission to use this command." });
+                return;
             }
-        });
 
-        if (found) {
-            await interaction.editReply({content: `Word is already in the language`});
-        } else {
-            // send an embed with a button to confirm the word
-            const embed = new EmbedBuilder()
-                .setTitle('Confirm Word') // @ts-ignore
-                .setDescription(`Are you sure you want to add ${interaction.options.getString('gufijskay')}(${interaction.options.getString('english')}) to the language?`)
-                .setColor(0x00ff00)
-
-            // make a button to confirm the word
-            const button = new ButtonBuilder()
-                .setCustomId('confirm')
-                .setLabel('Confirm')
-                .setStyle(ButtonStyle.Success);
-
-            // make a button to deny the word
-            const button2 = new ButtonBuilder()
-                .setCustomId('cancel')
-                .setLabel('Cancel')
-                .setStyle(ButtonStyle.Secondary);
-
-            // make a row for the buttons
-            const buttonRow = new ActionRowBuilder().addComponents(button, button2);
-
-            // @ts-ignore
-            await interaction.editReply({ embeds: [embed], components: [buttonRow], fetchReply: true }).then(async (msg) => {
-                // @ts-ignore
-                const filter = (interaction: MessageActionRowComponent) => interaction.user.id === interaction.user.id;
-                // @ts-ignore
-                const collector = msg.createMessageComponentCollector({ filter, time: 30000 });
-
-                // @ts-ignore
-                let toAdd = [`${interaction.options.getString('gufijskay')}`, `${interaction.options.getString('english')}`];
-                // @ts-ignore
-                if (interaction.options.getString('description')) {
-                    // @ts-ignore
-                    toAdd = [...toAdd, `${interaction.options.getString('description')}`];
-                }
-
-                let row = await sheet.addRow(toAdd);
-
-                collector.on('collect', async (i: ActionRowComponent) => {
-                    if (i.customId === 'confirm') {
-                        await row.save();
-                        await interaction.editReply({ content: `Added ${row._rawData[0]} to the language`, components: [], embeds: [] });
-                        collector.stop("confirmed");
-                    } else if (i.customId === 'cancel') {
-                        await row.delete();
-                        await interaction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
-                        collector.stop("cancelled");
-                    }
-                });
-
-                collector.on('end', async (collected, reason) => {
-                    if (reason === "confirmed" || reason === "cancelled") return;
-                    await row.delete();
-                    await interaction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
-                });
+            const doc = new GoogleSpreadsheet(process.env.CAVEMEN_WORDS_SID);
+            await doc.useServiceAccountAuth({
+                client_email: process.env.CLIENT_EMAIL,
+                private_key: process.env.PRIVATE_KEY,
             });
-        }
+
+            await doc.loadInfo();
+            const sheet = doc.sheetsByTitle["Cave man Gufijskay"];
+            const rows = await sheet.getRows();
+
+            // get the values from the modal
+            const gufijskay = modalInteraction.fields.getTextInputValue('gufijskay')
+            const english = modalInteraction.fields.getTextInputValue('english')
+            const description = modalInteraction.fields.getTextInputValue('description')
+
+
+            let found = false;
+            await rows.forEach((row: string) => {
+                // @ts-ignore
+                let data = row["_rawData"]
+                let searchableData = [ data[0], data[1] ]
+
+                // @ts-ignore
+                if (searchableData[0].toString().toLowerCase() === gufijskay.toLowerCase() || searchableData[1].toString().toLowerCase() === english.toLowerCase()) {
+                    found = true
+                }
+            });
+
+            if (found) {
+                await modalInteraction.editReply({ content: "The word already exists in the dictionary." });
+                return;
+            } else {
+                // send an embed with a button to confirm the word
+                const embed = new EmbedBuilder()
+                    .setTitle('Confirm Word') // @ts-ignore
+                    .setDescription(`Are you sure you want to add ${gufijskay}(${english}) to the language?`)
+                    .setColor(0x00ff00)
+
+                // make a button to confirm the word
+                const button = new ButtonBuilder()
+                    .setCustomId('confirm')
+                    .setLabel('Confirm')
+                    .setStyle(ButtonStyle.Success);
+
+                // make a button to deny the word
+                const button2 = new ButtonBuilder()
+                    .setCustomId('cancel')
+                    .setLabel('Cancel')
+                    .setStyle(ButtonStyle.Secondary);
+
+                // make a row for the buttons
+                const buttonRow = new ActionRowBuilder().addComponents(button, button2);
+
+                // @ts-ignore
+                await modalInteraction.editReply({ embeds: [embed], components: [buttonRow], fetchReply: true }).then(async (msg) => {
+                    // @ts-ignore
+                    const filter = (modalInteraction: MessageActionRowComponent) => modalInteraction.user.id === modalInteraction.user.id;
+                    // @ts-ignore
+                    const collector = msg.createMessageComponentCollector({ filter, time: 30000 });
+
+                    // @ts-ignore
+                    let toAdd = [`${gufijskay}`, `${english}`];
+                    // @ts-ignore
+                    if (description) {
+                        // @ts-ignore
+                        toAdd = [...toAdd, `${description}`];
+                    }
+
+                    let row = await sheet.addRow(toAdd);
+
+                    collector.on('collect', async (i: ActionRowComponent) => {
+                        if (i.customId === 'confirm') {
+                            await row.save();
+                            await modalInteraction.editReply({ content: `Added ${row._rawData[0]} to the language`, components: [], embeds: [] });
+                            collector.stop("confirmed");
+                        } else if (i.customId === 'cancel') {
+                            await row.delete();
+                            await modalInteraction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
+                            collector.stop("cancelled");
+                        }
+                    });
+
+                    collector.on('end', async (collected, reason) => {
+                        if (reason === "confirmed" || reason === "cancelled") return;
+                        await row.delete();
+                        await modalInteraction.editReply({ content: `Cancelled adding ${row._rawData[0]} to the language`, components: [], embeds: [] });
+                    });
+                });
+            }
+        })
     },
 };
 
